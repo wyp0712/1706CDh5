2fe43e0417208f24410bdbbba84b2ef80d026b18
devinwang <13263262903@163.com>
2019-07-26 16:34:50 +0800

    下拉刷新

---
+++ day16-mouth3/1706D/ChineseFinancial/libs/ajax.js
@@ -0,0 +1,13 @@
+function ajax(url, callback) {
+  var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP")
+  xhr.open("get", url, true);
+  xhr.send();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4) {
+      if (xhr.status == 200) {
+        var data = xhr.response;
+        callback && callback(JSON.parse(data))
+      }
+    }
+  }
+}
